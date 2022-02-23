f14c0af05937e238947390245bc76889c7c94dfb
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-02-23 12:32:38 +0900

    heroku: port 수정

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const webtoonsData = require("./data/webtoon.json");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/webtoons", (req, res) => {
   res.send(webtoonsData);
