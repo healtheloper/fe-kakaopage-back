b3132c8fad723776a09b71ce9cbca5043b83d8b1
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-02-23 14:30:18 +0900

    feat: 데이터 추가 및 확장자 변경

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const webtoonsData = require("./data/webtoon.json");
+const webtoonsData = require("./data/webtoons");
+const categoriesData = require("./data/categories");
+const genresData = require("./data/genres");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/genres", (req, res) => {
+  res.send(genresData);
+});
+app.get("/categories", (req, res) => {
+  res.send(categoriesData);
+});
 app.get("/webtoons", (req, res) => {
   res.send(webtoonsData);
 });
@@ -11,4 +19,6 @@ app.get("/webtoons", (req, res) => {
 app.listen(port, () => {
   console.log(`✅ Back Server Listening on port ${port}`);
   console.log(`  webtoons data → GET /webtoons `);
+  console.log(`  categories data → GET /categories `);
+  console.log(`  genres data → GET /genres `);
 });
